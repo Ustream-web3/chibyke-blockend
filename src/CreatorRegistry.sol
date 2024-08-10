3cc118f510f3f4351af34aaa9d22a3678d317f45
yeahChibyke <chibykepride@gmail.com>
2024-08-10 21:17:02 +0100

    working on CreatorRegistry contract

--- src/CreatorRegistry.sol
+++ src/CreatorRegistry.sol
@@ -1,16 +1,35 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
 pragma solidity ^0.8.24;
 
-contract CreatorRegistry {
-    // Pseudocode
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-    // CreatorProfile Struct
-    
-    // Function that creators call to be registered --> createCreatorProfile
-    // Perhaps, I should create a Content.sol smart contract that inherits the CreatorRegistry contract. In this new contract, creators will add their content to the UStream platform by calling a addContent function
-        // Should it be possible for an admin to be able to addContent on behalf of creators?
-    // For the adding(uploading) of content, I have an idea:
-        // Creators upload their content off-chain, this is to enable necessary checks to be performed by the UStream team. Approved content are issued a code which is inputted as a parameter in the addContent function
-        // If this is the case, perhaps only admin should be able to addContent
-        // Perhaps the Content.sol should Ownable from OpenZeppelin for more security in admin privilegdes
+contract CreatorRegistry is Ownable {
+    // >---------------------------> ERRORS
+    error CreatorRegistry__CreatorAlreadyApproved();
+
+    // >---------------------------> TYPE DECLARATIONS
+    struct CreatorProfile {
+        address creator;
+        uint256 creatorId;
+    }
+
+    // >---------------------------> STATE VARIABLES
+    uint256 creatorCount;
+    mapping(address => CreatorProfile) private s_creators;
+    mapping(address => bool) private s_creatorApproved;
+
+    // >---------------------------> EVENTS
+    event CreatorAdded(address indexed creator, uint256 indexed creatorId);
+
+    // >---------------------------> MODIFIERS
+    modifier creatorAlreadyAdded(address creator, uint256 creatorId) {
+        if (s_creatorApproved[creator]) {
+            revert CreatorRegistry__CreatorAlreadyApproved();
+            _;
+        }
+    }
+
+    // >---------------------------> CONSTRUCTOR
+    constructor() Ownable(msg.sender) {}
 }
