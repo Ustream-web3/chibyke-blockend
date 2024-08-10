29360d2ffdede57011055d5ad219148cc4e7990c
yeahChibyke <chibykepride@gmail.com>
2024-08-10 23:31:21 +0100

    more work done on CreatorRegistry contract

--- src/CreatorRegistry.sol
+++ src/CreatorRegistry.sol
@@ -6,8 +6,10 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CreatorRegistry is Ownable {
     // >---------------------------> ERRORS
-    error CreatorRegistry__CreatorAlreadyApproved();
     error CreatorRegistry__NotTheAdmin();
+    error CreatorRegistry__CreatorAlreadyAdded();
+    error CreatorRegistry__CreatorAlreadySuspended();
+    error CreatorRegistry__CreatorAlreadyRemoved();
 
     // >---------------------------> TYPE DECLARATIONS
     struct CreatorProfile {
@@ -21,13 +23,21 @@ contract CreatorRegistry is Ownable {
     mapping(address => CreatorProfile) private s_creators;
     mapping(address => bool) private s_creatorsApproved;
 
+    mapping(address => uint256) private s_creatorToCreatorId;
+    mapping(uint256 => address) private s_creatorIdToCreator;
+
+    address[] private s_suspendedCreators;
+    address[] private s_removedCreators;
+
     // >---------------------------> EVENTS
     event CreatorAdded(address indexed creator, uint256 indexed creatorId);
+    event CreatorSuspended(address indexed creator, uint256 indexed creatorId);
+    event CreatorRemoved(address indexed creator, uint256 indexed creatorId);
 
     // >---------------------------> MODIFIERS
     modifier creatorAlreadyAdded(address creator, uint256 creatorId) {
         if (s_creatorsApproved[creator]) {
-            revert CreatorRegistry__CreatorAlreadyApproved();
+            revert CreatorRegistry__CreatorAlreadyAdded();
         }
         _;
     }
@@ -58,5 +68,25 @@ contract CreatorRegistry is Ownable {
         creatorCount++;
     }
 
-    function removeCreator() external {}
+    /**
+     * @notice This function will suspend the creator with the inputted address and creatorId
+     */
+    function suspendCreator() external onlyAdmin {}
+
+    /**
+     * @notice This function will remove the creator with the inputted address and creatorId
+     */
+    function removeCreator() external onlyAdmin {}
+
+    // >---------------------------> EXTERNAL VIEW FUNCTIONS
+
+    /**
+     * @notice this function will return the creator address of the inputted creatorId
+     */
+    function getAddedCreator(uint256 creatorId) external view returns (address) {}
+
+    /**
+     * @notice This function will return the creatorId of the inputted creator address
+     */
+    function getAddedCreatorId(address creator) external view returns (uint256) {}
 }
