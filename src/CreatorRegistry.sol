b795f59dd2b18a1e132d7e6424ac71fc2207b2b0
yeahChibyke <chibykepride@gmail.com>
2024-08-20 13:35:34 +0100

    first refactor

--- src/CreatorRegistry.sol
+++ src/CreatorRegistry.sol
@@ -7,78 +7,53 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract CreatorRegistry is Ownable {
     // >---------------------------> ERRORS
     error CreatorRegistry__NotTheAdmin();
-    error CreatorRegistry__CreatorAlreadyAdded();
+    error CreatorRegistry__CreatorExists();
     error CreatorRegistry__CreatorDoesNotExist();
     error CreatorRegistry__CreatorAlreadySuspended();
     error CreatorRegistry__CreatorAlreadyRemoved();
 
-    // >---------------------------> TYPE DECLARATIONS
-    struct AddedCreator {
-        address creator;
-        uint256 creatorId;
-    }
-
-    struct SuspendedCreator {
-        address creator;
-        uint256 creatorId;
-    }
-
-    struct RemovedCreator {
-        address creator;
-        uint256 creatorId;
-    } // since the creator (and accompanying creatorId will be totally removed from the protocol, is there any need for this struct?)
-
     // >---------------------------> STATE VARIABLES
     address private s_admin;
     uint256 private s_creatorCount;
+    uint256 private s_suspendedCreatorCount;
+    uint256 private s_removedCreatorCount;
+    address[] private s_Creators;
+    address[] private s_suspendedCreators;
+    address[] private s_removedCreators;
 
-    mapping(address => AddedCreator) private s_addedCreators;
-    mapping(address => SuspendedCreator) private s_suspendedCreators;
-    mapping(address => RemovedCreator) private s_removedCreators;
-
-    mapping(address => bool) private s_isAddedCreator;
-    mapping(uint256 => bool) private s_isAddedCreatorId;
-
-    mapping(address => uint256) private s_creatorToCreatorId;
-    mapping(uint256 => address) private s_creatorIdToCreator;
-
+    mapping(address => bool) private s_isCreator;
     mapping(address => bool) private s_isSuspendedCreator;
-    mapping(uint256 => bool) private s_isSuspendedCreatorId;
-
     mapping(address => bool) private s_isRemovedCreator;
-    mapping(uint256 => bool) private s_isRemovedCreatorId;
 
     // >---------------------------> EVENTS
-    event CreatorAdded(address indexed creator, uint256 indexed creatorId, address indexed adminThatAddedCreator);
-    event CreatorSuspended(
-        address indexed creator, uint256 indexed creatorId, address indexed adminThatSuspendedCreator
-    );
-    event CreatorRemoved(address indexed creator, uint256 indexed creatorId, address indexed adminThatRemovedCreator);
+    event CreatorAdded(address indexed creator, address indexed adminThatAddedCreator);
+    event CreatorSuspended(address indexed creator, address indexed adminThatSuspendedCreator);
+    event CreatorRemoved(address indexed creator, address indexed adminThatRemovedCreator);
 
     // >---------------------------> MODIFIERS
-    modifier alreadyAdded(address creator, uint256 creatorId) {
-        if (s_isAddedCreator[creator] && s_isAddedCreatorId[creatorId]) {
-            revert CreatorRegistry__CreatorAlreadyAdded();
-        }
-        _;
-    }
-
-    modifier alreadyExists(address creator, uint256 creatorId) {
-        if (!s_isAddedCreator[creator] && !s_isAddedCreatorId[creatorId]) {
+    modifier alreadyCreator(address creator) {
+        if (!s_isCreator[creator]) {
             revert CreatorRegistry__CreatorDoesNotExist();
         }
         _;
     }
 
-    modifier notAlreadySuspended(address creator, uint256 creatorId) {
-        if (s_isSuspendedCreator[creator] && s_isSuspendedCreatorId[creatorId]) {
+    modifier notAlreadyCreator(address applicant) {
+        if (s_isCreator[applicant]) {
+            revert CreatorRegistry__CreatorExists();
+        }
+        _;
+    }
+
+    modifier notAlreadySuspended(address creator) {
+        if (s_isSuspendedCreator[creator]) {
             revert CreatorRegistry__CreatorAlreadySuspended();
         }
         _;
     }
 
-    modifier notAlreadyRemoved(address creator, uint256 creatorId) {
-        if (s_isRemovedCreator[creator] && s_isRemovedCreatorId[creatorId]) {
+    modifier notAlreadyRemoved(address creator) {
+        if (s_isRemovedCreator[creator]) {
             revert CreatorRegistry__CreatorAlreadyRemoved();
         }
         _;
@@ -91,91 +66,115 @@ contract CreatorRegistry is Ownable {
         _;
     }
 
-    // >---------------------------> CONSTRUCTOR
+    // >---------------------------> CONSTRUCTOR 
     constructor() Ownable(msg.sender) {
         s_admin = msg.sender;
         s_creatorCount = 0;
+        s_suspendedCreatorCount = 0;
+        s_removedCreatorCount = 0;
     }
 
     // >---------------------------> EXTERNAL FUNCTIONS
-    function addCreator(address creator, uint256 creatorId) external onlyAdmin alreadyAdded(creator, creatorId) {
-        s_addedCreators[creator] = AddedCreator(creator, creatorId);
-        s_isAddedCreator[creator] = true;
-        s_isAddedCreatorId[creatorId] = true;
+    function addCreator(address applicant) external onlyAdmin notAlreadyCreator(applicant) {
+        // add to Creators
+        s_Creators.push(applicant);
 
-        emit CreatorAdded(creator, creatorId, msg.sender);
+        // mark as Creator
+        s_isCreator[applicant] = true;
+
+        // emit event to effect
+        emit CreatorAdded(applicant, msg.sender);
 
         s_creatorCount++;
     }
 
     /**
-     * @notice This function will suspend the creator with the inputted address and creatorId
+     * @notice This function will suspend the creator with the inputted address
      */
-    function suspendCreator(address creator, uint256 creatorId)
+    function suspendCreator(address creator)
         external
         onlyAdmin
-        alreadyExists(creator, creatorId)
-        notAlreadySuspended(creator, creatorId)
-        notAlreadyRemoved(creator, creatorId)
+        alreadyCreator(creator)
+        notAlreadySuspended(creator)
+        notAlreadyRemoved(creator)
     {
-        // I should add a check that ensures that the inputted pair of address and id actually match. Can a keccakk do this?
+        // remove from Creators
+        s_Creators.pop();
+        s_isCreator[creator] = false;
 
-        // remove from added creators
-        delete s_addedCreators[creator];
-        delete s_isAddedCreator[creator];
-        delete s_isAddedCreatorId[creatorId];
+        // add to suspended Creators
+        s_suspendedCreators.push(creator);
 
-        // add to suspended creators
-        s_suspendedCreators[creator] = SuspendedCreator(creator, creatorId);
+        // mark as suspended Creator
         s_isSuspendedCreator[creator] = true;
-        s_isSuspendedCreatorId[creatorId] = true;
 
-        emit CreatorSuspended(creator, creatorId, msg.sender);
+        // emit event to effect
+        emit CreatorSuspended(creator, msg.sender);
 
+        // decrease Creator count
         s_creatorCount--;
+
+        // increase suspended Creator count
+        s_suspendedCreatorCount++;
     }
 
     /**
-     * @notice This function will remove the creator with the inputted address and creatorId
+     * @notice This function will remove the creator with the inputted address
      */
-    function removeCreator(address creator, uint256 creatorId)
-        external
-        onlyAdmin
-        alreadyExists(creator, creatorId)
-        notAlreadyRemoved(creator, creatorId)
-    {
-        // Remove from added creators
-        delete s_addedCreators[creator];
-        delete s_isAddedCreator[creator];
-        delete s_isAddedCreatorId[creatorId];
+    function removeCreator(address creator) external onlyAdmin alreadyCreator(creator) notAlreadyRemoved(creator) {
+        if (s_isSuspendedCreator[creator]) {
+            // remove from suspended Creators
+            s_suspendedCreators.pop();
+            s_isSuspendedCreator[creator] = false;
 
-        // Mark as removed
-        s_removedCreators[creator] = RemovedCreator(creator, creatorId);
-        s_isRemovedCreator[creator] = true;
-        s_isRemovedCreatorId[creatorId] = true;
+            // add to removed Creators
+            s_removedCreators.push(creator);
+
+            // mark as removed Creator
+            s_isRemovedCreator[creator] = true;
+
+            // decrease suspended Creators count
+            s_suspendedCreatorCount--;
+        } else {
+            // remove from Creators
+            s_Creators.pop();
+            s_isCreator[creator] = false;
+
+            // add to removed Creators
+            s_removedCreators.push(creator);
+
+            // mark as removed Creator
+            s_isRemovedCreator[creator] = true;
+        }
+
+        // emit event to effect
+        emit CreatorRemoved(creator, msg.sender);
+
+        // decrease Creator count
+        s_creatorCount--;
+
+        // increase removed Creator count
+        s_removedCreatorCount++;
+    }
+    
+    function setAdmin(address newAdmin) external onlyAdmin {
+        s_admin = newAdmin;
     }
 
     // >---------------------------> PUBLIC VIEW FUNCTIONS
-
-    /**
-     * @notice this function will return the creator address of the inputted creatorId
-     */
-    function getCreator(uint256 creatorId) public view returns (address) {
-        return s_creatorIdToCreator[creatorId];
-    }
-
-    /**
-     * @notice This function will return the creatorId of the inputted creator address
-     */
-    function getCreatorId(address creator) public view returns (uint256) {
-        return s_creatorToCreatorId[creator];
-    }
-
     function getAdmin() public view returns (address) {
         return s_admin;
     }
 
     function getCurrentCreatorCount() public view returns (uint256) {
         return s_creatorCount;
+    }
+
+    function getSuspendedCreatorCount() public view returns (uint256) {
+        return s_suspendedCreatorCount;
+    }
+
+    function getRemovedCreatorCount() public view returns (uint256) {
+        return s_removedCreatorCount;
     }
 }
