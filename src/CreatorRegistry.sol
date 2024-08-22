d5ea34289e2e88c72b4557bcded627400837a21f
yeahChibyke <chibykepride@gmail.com>
2024-08-22 09:54:44 +0100

    check dump.md for dump marked ###22/08/2024

--- src/CreatorRegistry.sol
+++ src/CreatorRegistry.sol
@@ -196,21 +196,4 @@ contract CreatorRegistry is Ownable {
         array.pop();
         delete indexMapping[element];
     }
-
-    // function _updateCreatorState(address creator, CreatorState newState) internal {
-    //     CreatorState currentState = s_creatorState[creator];
-
-    //     require(currentState != newState, "Cannot transition to same state!!!");
-
-    //     if (newState == CreatorState.Suspended) {
-    //         require(currentState == CreatorState.Active, "Can only suspend active creators!!!");
-    //     } else if (newState == CreatorState.Removed) {
-    //         require(
-    //             currentState == CreatorState.Active || currentState == CreatorState.Suspended,
-    //             "Can only remove active or suspended creators!!!"
-    //         );
-    //     }
-
-    //     s_creatorState[creator] = newState;
-    // } @note Is there a need for this function? SHould there ever arise a situation where a creator's state need to be updated without any of the `addCreator()` `suspendCreator()` removeCreator()` functions being called?
 }
