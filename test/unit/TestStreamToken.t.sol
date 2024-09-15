aa356aa90b7e7ad9dc21be44fa51bd08c34075ae
yeahChibyke <chibykepride@gmail.com>
2024-09-15 18:30:11 +0100

    refactored token contract in preparation of airdrop contract

--- test/unit/TestStreamToken.t.sol
+++ test/unit/TestStreamToken.t.sol
@@ -7,61 +7,12 @@ import {StreamToken} from "../../src/StreamToken.sol";
 contract TestStreamToken is Test {
     StreamToken streamToken;
 
-    address guardian = makeAddr("guardian");
-    address Chibyke = makeAddr("Chibyke");
-    address David = makeAddr("David");
-
     function setUp() external {
-        streamToken = new StreamToken(guardian);
+        streamToken = new StreamToken();
     }
 
     function testConstructor() public view {
         assertEq(streamToken.name(), "Stream Token");
         assertEq(streamToken.symbol(), "STRK");
-    }
-
-    function testGuardian() public view {
-        assertEq(streamToken.guardian(), guardian);
-    }
-
-    function testRevertIfNotGuardianMint() public {
-        vm.prank(Chibyke);
-        vm.expectRevert(StreamToken.StreamToken__NotGuardian.selector);
-        streamToken.mint(David, 10);
-
-        assert(streamToken.balanceOf(David) == 0);
-    }
-
-    function testRevertIfMintAmountIsZero() public {
-        vm.prank(guardian);
-        vm.expectRevert(StreamToken.StreamToken__MustBeMoreThanZero.selector);
-        streamToken.mint(David, 0);
-    }
-
-    function testRevertIfReceiverIsZeroAddress() public {
-        vm.prank(guardian);
-        vm.expectRevert();
-        streamToken.mint(address(0), 10);
-    }
-
-    function testMintSuccessful() public {
-        vm.prank(guardian);
-        streamToken.mint(David, 10);
-
-        assert(streamToken.balanceOf(David) == 10);
-    }
-
-    function testCanSetGuardian() public {
-        streamToken.setGuardian(Chibyke);
-
-        assert(streamToken.guardian() == Chibyke);
-    }
-
-    function testOnlyOwnerCanSetGuardian() public {
-        vm.prank(guardian);
-        vm.expectRevert();
-        streamToken.setGuardian(Chibyke);
-
-        assert(streamToken.guardian() != Chibyke);
     }
 }
